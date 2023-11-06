a39586b0089461754924f62596c42d20d0398d61
Carlos A Santos <carlos.itdeveloper@gmail.com>
2023-11-06 04:54:32 -0300

    Install Microsoft.AspNetCore.Authorization and configure AddControllersWithViews

--- src/SmartAdmin.WebUI/Startup.cs
+++ src/SmartAdmin.WebUI/Startup.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -55,6 +57,15 @@ namespace SmartAdmin.WebUI
                 options.Password.RequireLowercase = true;
                 options.Password.RequireUppercase = true;
 
+            });
+
+            // Attibuto Global de Autorização
+            services.AddControllersWithViews(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                       .RequireAuthenticatedUser()
+                       .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
             });
 
 
