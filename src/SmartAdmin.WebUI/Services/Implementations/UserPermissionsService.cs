6a481fd47752e82cc08fa49ae8143b22bfb47efb
Carlos A Santos <carlos.itdeveloper@gmail.com>
2023-11-13 18:36:45 -0300

    Add UserPermissionsService

--- src/SmartAdmin.WebUI/Services/Implementations/UserPermissionsService.cs
+++ src/SmartAdmin.WebUI/Services/Implementations/UserPermissionsService.cs
@@ -1,8 +1,49 @@
-﻿using SmartAdmin.WebUI.Services.Abstractions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SmartAdmin.WebUI.Data;
+using SmartAdmin.WebUI.Models;
+using SmartAdmin.WebUI.Services.Abstractions;
 
 namespace SmartAdmin.WebUI.Services.Implementations
 {
     public class UserPermissionsService : IUserPermissionsService
     {
+        private readonly ApplicationDbContext _context;
+
+        public UserPermissionsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<UserPermission>> GetUserPermissionsAsync(string userId)
+        {
+            return await _context.UserPermissions
+                .Where(up => up.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task AddPermissionAsync(UserPermission permission)
+        {
+            _context.UserPermissions.Add(permission);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemovePermissionAsync(UserPermission permission)
+        {
+            _context.UserPermissions.Remove(permission);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> HasPermissionAsync(string userId, string controllerName, string actionName)
+        {
+            return await _context.UserPermissions.AnyAsync(up =>
+                up.UserId == userId &&
+                up.ControllerName == controllerName &&
+                up.ActionName == actionName);
+        }
+
+        // Implementar outros métodos conforme necessário
     }
 }
