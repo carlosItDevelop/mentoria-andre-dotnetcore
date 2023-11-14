0495db04603d1fab4601530e7bbcd272114c9a2f
Carlos A Santos <carlos.itdeveloper@gmail.com>
2023-11-14 09:25:35 -0300

    Delete features to permissiosn based in roles

--- src/SmartAdmin.WebUI/Data/ApplicationDbContext.cs
+++ src/SmartAdmin.WebUI/Data/ApplicationDbContext.cs
@@ -11,7 +11,6 @@ namespace SmartAdmin.WebUI.Data
             : base(options){}
 
         public DbSet<Funcionario> Funcionarios { get; set; }
-        public DbSet<UserPermission> UserPermissions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
--- src/SmartAdmin.WebUI/Services/Abstractions/IUserPermissionsService.cs
+++ src/SmartAdmin.WebUI/Services/Abstractions/IUserPermissionsService.cs
@@ -1,15 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using SmartAdmin.WebUI.Models;
-
-namespace SmartAdmin.WebUI.Services.Abstractions
+﻿namespace SmartAdmin.WebUI.Services.Abstractions
 {
     public interface IUserPermissionsService
     {
-        Task<IEnumerable<UserPermission>> GetUserPermissionsAsync(string userId);
-        Task AddPermissionAsync(UserPermission permission);
-        Task RemovePermissionAsync(UserPermission permission);
-        Task<bool> HasPermissionAsync(string userId, string controllerName, string actionName);
-        // Outros métodos conforme necessário
     }
 }
